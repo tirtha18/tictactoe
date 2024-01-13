53c4e8f9ba05ea241e19aa36d9eb860609d1a39b
unknown <tirthamunai1234@gmail.com>
2024-01-13 20:33:25 +0530

    Changes

--- src/components/Winner.jsx
+++ src/components/Winner.jsx
@@ -10,7 +10,7 @@ export default function Winner(props) {
                 <div className="text-neutral-800 text-2xl font-medium font-['Inter'] capitalize">Won</div>
             </div>
         <div className="w-[312px] text-center text-neutral-500 text-base font-normal font-['Inter'] leading-normal">Congrats on being the undisputed champion of pressing buttons like a pro.</div></div>
-        <div onClick={() => {window.location.reload();}} className="w-[179px] h-[72px] px-12 py-[18px] bg-sky-700 rounded-[56px] shadow justify-center items-center gap-2 inline-flex mt-9 mb-6">
+        <div onClick={() => {window.location.reload();}} className="hover:scale-105 cursor-pointer w-[179px] h-[72px] px-12 py-[18px] bg-sky-700 rounded-[56px] shadow justify-center items-center gap-2 inline-flex mt-9 mb-6">
             <div className="text-white text-base font-bold font-['Inter'] uppercase leading-tight tracking-wider">Restart</div>
         </div>
     </div>
